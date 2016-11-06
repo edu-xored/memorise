48c0f87db403785e38d9c13de709df570d323adb
firix-pavel <firix-pavel@yandex.ru>
2016-11-06 19:42:42 +0700

    rename QuartzJob to MemoCrawlerJob and add logging.

--- memorise-web-app/src/main/java/org/xored/edu/memorise/MemoCrawlerJob.java
+++ memorise-web-app/src/main/java/org/xored/edu/memorise/MemoCrawlerJob.java
@@ -4,9 +4,16 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
-public class QuartzJob extends QuartzJobBean{
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MemoCrawlerJob extends QuartzJobBean{
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        logger.info("MemoCrawlerJob is running");
         //TODO: implement job
     }
 }

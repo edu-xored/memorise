501741604e286f363a4f40f07be1629fc3959808
firix-pavel <firix-pavel@yandex.ru>
2016-11-27 15:00:35 +0700

    add simple PropertyReader

--- memorise-web-app/src/main/java/org/xored/edu/memorise/PropertyReader.java
+++ memorise-web-app/src/main/java/org/xored/edu/memorise/PropertyReader.java
@@ -1,7 +1,45 @@
 package org.xored.edu.memorise;
 
-/**
- * Created by firix on 11/26/16.
- */
+import org.quartz.CronScheduleBuilder;
+import org.quartz.CronTrigger;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
 public class PropertyReader {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Value("${quartz.time}")
+	private String quartzTriggerTime;
+
+	@Autowired
+	private CronTriggerFactoryBean oldTrigger;
+
+	@Autowired
+	private SchedulerFactoryBean scheduler;
+
+	@PostConstruct
+	public void setupQuartz() {
+		System.out.println(quartzTriggerTime);
+
+		TriggerBuilder<CronTrigger> tb = oldTrigger.getObject().getTriggerBuilder();
+		Trigger newTrigger = tb.withSchedule(CronScheduleBuilder.cronSchedule(quartzTriggerTime)).build();
+
+		try {
+			scheduler.getObject().rescheduleJob(oldTrigger.getObject().getKey(), newTrigger);
+		} catch (Exception SchedulerException) {
+			logger.info("ERROR: bad cron expression (quartz.time) in application properties");
+		}
+	}
+
 }
